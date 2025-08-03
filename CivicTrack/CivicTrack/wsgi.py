ce624084271f7a567b770f93a6541d9662d55b17
appalla-karthik <appallakarthik07@gmail.com>
2025-08-04 01:17:17 +0530

    Update wsgi.py

--- CivicTrack/CivicTrack/wsgi.py
+++ CivicTrack/CivicTrack/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CivicTrack.settings')
 
 from django.core.wsgi import get_wsgi_application
 
