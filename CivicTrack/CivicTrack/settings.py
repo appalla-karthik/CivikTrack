d574094f25fa71dc13a0215e926a24c9e8485c06
appalla-karthik <appallakarthik07@gmail.com>
2025-08-04 08:52:53 +0530

    Updated default settings.py

--- CivicTrack/CivicTrack/settings.py
+++ CivicTrack/CivicTrack/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'CivikTrack.CivicTrack.urls'
+ROOT_URLCONF = 'CivicTrack.urls'
 
 TEMPLATES = [
     {
